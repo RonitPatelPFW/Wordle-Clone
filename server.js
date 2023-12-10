b1b4723d3a3df6615f27b46bb0a31149157aca21
Ronit Patel <ronitpatel884@gmail.com>
2023-12-10 15:01:09 -0500

    file restructure again

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+    app = express()
+    expressLayouts = require('express-ejs-layouts')
+
+const port = 8080
+
+app.use(express.static(__dirname + '/public'))
+
+// set ejs as our templating engine
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
+// set the routes
+app.use(require('./app/routes'))
+
+// start our server
+app.listen(port, () => {
+    console.log(`App listening on http://localhost: ${port}`)
+})
