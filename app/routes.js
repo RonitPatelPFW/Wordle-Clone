8a8cb9132f3d8bca35158fc5bf33c37755f799ae
Ronit Patel <ronitpatel884@gmail.com>
2023-12-11 17:45:39 -0500

    got fetch req to json

--- app/routes.js
+++ app/routes.js
@@ -8,3 +8,15 @@ module.exports = router
 // define routes
 // main routes
 router.get('/', mainController.showHome)
+
+router.get('/fetchData', async (req, res) => {
+    try {
+      const response = await fetch(`https://www.nytimes.com/svc/wordle/v2/${req.query.date}.json`);
+      const data = await response.json();
+      res.json(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
