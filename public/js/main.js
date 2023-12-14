d79cfb6cc95a49447b6ba6c864d23f3863bdff86
Ronit Patel <ronitpatel884@gmail.com>
2023-12-14 09:26:25 -0600

    final commit

--- public/js/main.js
+++ public/js/main.js
@@ -94,27 +94,79 @@ function getAnswer() {
     });
 }
 getAnswer()
+
+function changeColor(str, color) {
+    for (i = 0; i < len; i++) {
+        if(letter[i].nodeName === "DIV") {
+            if(letter[i].textContent === str) {
+                letter[i].style.backgroundColor = color;
+                // break
+            }
+        }
+    }
+}
+
 function final_answer() {
+    //if user got answer
     if(finalStr === WORDLE_ANSWER) {
         changeRow(rowcount)
         document.body.style.pointerEvents = "none";
         document.removeEventListener('keydown', keyInputEventListener)
-        document.querySelector(".alert.alert-success").removeAttribute("hidden");
-        setTimeout(() => {
-            document.querySelector(".alert.alert-success").style.display = "none";
-        }, 3000)
+        for (i = 0; i < len; i++) {
+            if(letter[i].nodeName === "DIV") {
+                letter[i].style.backgroundColor = "green";
+            }
+        }
+        
+        $('.modal-body').html("You won!!");
+        $('#staticBackdrop').modal('show'); 
     }
     else{
+        //if user has 0 attempts remaing
         if(rowcount+1 === totalGuessRow) {
+            for (let i = 0; i < WORDLE_ANSWER.length; i++) {
+                for (let j = 0; j < finalStr.length; j++) {
+                    //check if letter at index are both same
+                    if((finalStr[j] === WORDLE_ANSWER[i]) && (i === j)) {                        
+                        changeColor(finalStr[j].toUpperCase(), "green")
+                    }
+                    else if((finalStr[j] === WORDLE_ANSWER[i])) {
+                        changeColor(finalStr[j].toUpperCase(), "rgb(211, 184, 12)")   
+                    }   
+                }     
+            }
+            for (let index = 0; index < finalStr.length; index++) {
+                console.log(finalStr[index])
+                    if(WORDLE_ANSWER.includes(finalStr[index]) === false) {
+                        changeColor(finalStr[index].toUpperCase(), "grey")   
+                    }
+            }
             changeRow(rowcount)
             document.body.style.pointerEvents = "none";
             document.removeEventListener('keydown', keyInputEventListener)
-            document.querySelector(".alert.alert-danger").removeAttribute("hidden");
-            setTimeout(() => {
-                document.querySelector(".alert.alert-danger").style.display = "none";
-            }, 3000)
+            $('.modal-body').html(`Better Luck Next Time<br>Answer: ${WORDLE_ANSWER}`);
+            $('#staticBackdrop').modal('show'); 
         }
         else {
+            //still going
+            for (let i = 0; i < WORDLE_ANSWER.length; i++) {
+                for (let j = 0; j < finalStr.length; j++) {
+                    //check if letter at index are both same
+                    if((finalStr[j] === WORDLE_ANSWER[i]) && (i === j)) {                        
+                        changeColor(finalStr[j].toUpperCase(), "green")
+                    }
+                    else if((finalStr[j] === WORDLE_ANSWER[i])) {
+                        changeColor(finalStr[j].toUpperCase(), "rgb(211, 184, 12)")   
+                    }
+                    
+                }
+            }
+            for (let index = 0; index < finalStr.length; index++) {
+                console.log(finalStr[index])
+                    if(WORDLE_ANSWER.includes(finalStr[index]) === false) {
+                        changeColor(finalStr[index].toUpperCase(), "grey")   
+                    }
+            }
             rowcount++
             changeRow(rowcount)
         }
@@ -122,4 +174,3 @@ function final_answer() {
    
     return false
 }
-
