27461d9ac5d24edeaf79ce4979d40d2fcd43afe3
Ronit Patel <ronitpatel884@gmail.com>
2023-12-14 19:59:27 -0600

    added game mode + other stuff

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express'),
     expressLayouts = require('express-ejs-layouts')
-    cors = require('cors')
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    opn = require('opn'); // Import the 'opn' package
+
 
 const app = express();
 const port = 8080;
+
+// use body parser to grab info from a form
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
@@ -15,6 +21,8 @@ app.use(require('./app/routes'));
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
+    opn(`http://localhost:${port}/`);
+
 });
 
 module.exports = app;
